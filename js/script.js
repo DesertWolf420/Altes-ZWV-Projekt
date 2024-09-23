b9c7d121e9e038cafe5934ba8d2ddc7811341ae1
Frank Schreiner <schreiner@suse.de>
2024-09-23 19:33:09 +0000

    anew tabase field 'board' and fchange filter in frontend

--- js/script.js
+++ js/script.js
@@ -26,13 +26,13 @@ function updateFlights() {
   const tableBody = document.querySelector('#flight-table tbody');
 
   var urlParams = new URLSearchParams(window.location.search);
-  var loc = urlParams.get('location');
+  var board = urlParams.get('board');
 
-  if (loc == null) {
-    loc = 'landshut'
+  if (board == null) {
+    board = 'Landshut'
   }
 
-  var flights_url = 'https://zwv.samaxi.de/rest/flights/' + loc + '.json';
+  var flights_url = 'https://zwv.samaxi.de/rest/flights.json';
 
   $.getJSON(flights_url, function(data) {
     flights = data;
@@ -43,49 +43,51 @@ function updateFlights() {
 
     tableBody.innerHTML = '';
     flights.forEach((flight, index) => {
-      const row = document.createElement('tr');
-      const flightTime = flight.time;
-      const [flightHours, flightMinutes] = flight.time.split(':');
-      const flightDateTime = new Date();
-      flightDateTime.setHours(flightHours);
-      flightDateTime.setMinutes(flightMinutes);
-      const timeDifferenceInMinutes = Math.floor((now - flightDateTime) / 60000);
+      if (flight.board == board ) {
+	const row = document.createElement('tr');
+	const flightTime = flight.time;
+	const [flightHours, flightMinutes] = flight.time.split(':');
+	const flightDateTime = new Date();
+	flightDateTime.setHours(flightHours);
+	flightDateTime.setMinutes(flightMinutes);
+	const timeDifferenceInMinutes = Math.floor((now - flightDateTime) / 60000);
 
-      if (orangeTimestamps[index]) {
-	const elapsedTimeSinceOrange = (now - orangeTimestamps[index]) / (1000 * 60 * 60);
-	if (elapsedTimeSinceOrange >= 24) {
-	  rowMarkedForOrange[index] = false;
-	  orangeTimestamps[index] = null;
+	if (orangeTimestamps[index]) {
+	  const elapsedTimeSinceOrange = (now - orangeTimestamps[index]) / (1000 * 60 * 60);
+	  if (elapsedTimeSinceOrange >= 24) {
+	    rowMarkedForOrange[index] = false;
+	    orangeTimestamps[index] = null;
+	  }
 	}
-      }
 
-      let rowClass = '';
-      if (rowStatus[index] === "green") {
-	rowClass = 'green-row';
-      } else if (rowStatus[index] === "red") {
-	rowClass = 'red-row';
-      } else if (rowMarkedForOrange[index]) {
-	rowClass = 'orange';
-      } else if (timeDifferenceInMinutes > 5) {
-	rowMarkedForOrange[index] = true;
-	orangeTimestamps[index] = new Date();
-	rowClass = 'orange';
-      } else if (flightTime === currentTime) {
-	rowClass = 'blink';
-      }
+	let rowClass = '';
+	if (rowStatus[index] === "green") {
+	  rowClass = 'green-row';
+	} else if (rowStatus[index] === "red") {
+	  rowClass = 'red-row';
+	} else if (rowMarkedForOrange[index]) {
+	  rowClass = 'orange';
+	} else if (timeDifferenceInMinutes > 5) {
+	  rowMarkedForOrange[index] = true;
+	  orangeTimestamps[index] = new Date();
+	  rowClass = 'orange';
+	} else if (flightTime === currentTime) {
+	  rowClass = 'blink';
+	}
 
-      row.innerHTML = `
-	<td class="${rowClass}">${flight.time}</td>
-	<td class="${rowClass}">${flight.destination}</td>
-	<td class="${rowClass}">${flight.flight}</td>
-	<td class="${rowClass}">${rowStatus[index]}</td>
-	<td class="${rowClass}">
-	  <button onclick="markAsDone(${index}, 'green')">Erledigt</button>
-	  <button onclick="markAsDone(${index}, 'red')">nicht erledigt</button>
-	  <button onclick="resetStatus(${index})">Zurücksetzen</button>
-	</td>
-      `;
-      tableBody.appendChild(row);
+	row.innerHTML = `
+	  <td class="${rowClass}">${flight.time}</td>
+	  <td class="${rowClass}">${flight.destination}</td>
+	  <td class="${rowClass}">${flight.flight}</td>
+	  <td class="${rowClass}">${rowStatus[index]}</td>
+	  <td class="${rowClass}">
+	    <button onclick="markAsDone(${index}, 'green')">Erledigt</button>
+	    <button onclick="markAsDone(${index}, 'red')">nicht erledigt</button>
+	    <button onclick="resetStatus(${index})">Zurücksetzen</button>
+	  </td>
+	`;
+	tableBody.appendChild(row);
+      }
     });
   });
 }
